e997f9401b8dbe4e62c4a71a973f789cb45b3800
Kaustubh Singh <kaustubh.singh@gmail.com>
2013-12-27 20:03:18 -0800

    Async read js

---
+++ Project Euler/Problem 18/maximum_path_sum_1_ASYNC_READ.js
@@ -0,0 +1,52 @@
+// usage:  node maximum_path_sum_1_ASYNC_READ.js <input file>
+
+function max_path_sum_recurse(triangle, level, index, max_level) {
+	
+	if (level > max_level) return 0;
+	
+	var max_sum = 0;
+	
+	max_sum = triangle[index -1 + (level - 1) * level / 2] +
+	 Math.max(max_path_sum_recurse(triangle, level + 1, index, max_level),
+				max_path_sum_recurse(triangle, level + 1, index + 1, max_level));	
+
+	return max_sum;
+}
+
+
+var max_path_sum = function() {
+
+	var fs = require("fs");
+	
+	fs.readFile(process.argv[2], function(err, data) {
+
+				if (err) throw err;
+								
+				var triangle = data.toString();			
+								
+				var level = 0;
+				for (var i = 0; i < triangle.length; ++i)
+				{
+						if (triangle[i] == "\n") level++;
+				}
+				
+				console.log(level);
+				
+				triangle = triangle.split(/ |\n/);
+				triangle.pop();
+				triangle = triangle.map( function(x){
+					return parseInt(x, 10);
+				});
+	
+				var answer = max_path_sum_recurse(triangle, 1, 1, level);
+				
+				console.log(answer);
+				console.log("time taken in ms: %d", (new Date()).getTime() - start_time);
+				
+			});
+}
+
+start_time = (new Date()).getTime();
+max_path_sum();
+
+
