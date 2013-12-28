278a3c0645ad70a8da627b902339a9c8d386ed1d
Kaustubh Singh <kaustubh.singh@gmail.com>
2013-12-27 21:36:41 -0800

    Fixing callback hell

--- Project Euler/Problem 18/maximum_path_sum_1_ASYNC_READ.js
+++ Project Euler/Problem 18/maximum_path_sum_1_ASYNC_READ.js
@@ -15,37 +15,37 @@ function max_path_sum_recurse(triangle, level, index, max_level) {
 	return max_sum;
 }
 
+var callback = function(err, data) {
 
+	if (err) throw err;
+					
+	var triangle = data.toString();			
+					
+	var level = 0;
+	for (var i = 0; i < triangle.length; ++i)
+	{
+			if (triangle[i] == "\n") level++;
+	}
+	
+	console.log(level);
+	
+	triangle = triangle.split(/ |\n/);
+	triangle.pop();
+	triangle = triangle.map( function(x){
+		return parseInt(x, 10);
+	});
+
+	var answer = max_path_sum_recurse(triangle, 1, 1, level);
+	
+	console.log(answer);
+	console.log("time taken in ms: %d", (new Date()).getTime() - start_time);	
+}
+			
 var max_path_sum = function() {
 
 	var fs = require("fs");
 	
-	fs.readFile(process.argv[2], function(err, data) {
-
-				if (err) throw err;
-								
-				var triangle = data.toString();			
-								
-				var level = 0;
-				for (var i = 0; i < triangle.length; ++i)
-				{
-						if (triangle[i] == "\n") level++;
-				}
-				
-				console.log(level);
-				
-				triangle = triangle.split(/ |\n/);
-				triangle.pop();
-				triangle = triangle.map( function(x){
-					return parseInt(x, 10);
-				});
-	
-				var answer = max_path_sum_recurse(triangle, 1, 1, level);
-				
-				console.log(answer);
-				console.log("time taken in ms: %d", (new Date()).getTime() - start_time);
-				
-			});
+	fs.readFile(process.argv[2], callback);
 }
 
 start_time = (new Date()).getTime();
