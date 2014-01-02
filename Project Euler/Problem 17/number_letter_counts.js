1ac26f1c9f6f8cb2a518445d963497a03472bcd1
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-01-01 21:36:08 -0800

    Implemented extract_digits

--- Project Euler/Problem 17/number_letter_counts.js
+++ Project Euler/Problem 17/number_letter_counts.js
@@ -3,5 +3,19 @@
 word_map = { 1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five", 6 : "six",
 				7 : "seven", 8 : "eight", 9 : "nine" };
 
-console.log(word_map['5']);
+// console.log(word_map['5']);
+
+function extract_digits(n)
+{
+	var digits = [];
+	
+	while (n > 0)
+	{
+		digits.unshift(n % 10);
+		n = Math.floor(n / 10);
+	}
+	return digits;
+}
+
+console.log(extract_digits(324));
 
