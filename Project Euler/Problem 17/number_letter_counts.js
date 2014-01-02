6b7057e67c377bcd959a69f1207d08fcdd50e222
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-01-01 21:08:12 -0800

    Defined number to word mapping

---
+++ Project Euler/Problem 17/number_letter_counts.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+word_map = { 1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five", 6 : "six",
+				7 : "seven", 8 : "eight", 9 : "nine" };
+
+console.log(word_map['5']);
+
