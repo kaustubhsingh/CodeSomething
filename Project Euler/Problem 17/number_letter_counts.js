f2428897e2f1242f4d6afda658dbb99e0f2e4402
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-01-02 21:38:33 -0800

    Dev the html app

--- Project Euler/Problem 17/number_letter_counts.js
+++ Project Euler/Problem 17/number_letter_counts.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 
 word_map = { 1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five", 6 : "six",
 			 7 : "seven", 8 : "eight", 9 : "nine", 10 : "ten" , 11 : "eleven", 12 : "twelve",
@@ -123,8 +122,11 @@ function number_to_words_helper(n)
 
 function number_to_words(n)
 {
-	number_to_words_helper(extract_digits(n));
+	alert("hi");
+	console.log(arguments.callee.name);
+	alert("hi"); document.write("hihi");
+	alert(number_to_words_helper(extract_digits(n)));
 }
 
-console.log(number_to_words_helper([4, 5, 1]));
+//console.log(number_to_words_helper([4, 5, 1]));
 
