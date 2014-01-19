8d579a6801f23b993fad444d08babf19937acc63
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-01-18 23:11:53 -0800

    added isLeapYear()

--- Project Euler/Problem 19/counting_sundays.js
+++ Project Euler/Problem 19/counting_sundays.js
@@ -3,4 +3,24 @@
 month_days = { 1 : 31, 3 : 31, 4 : 30, 5 : 31, 6 : 30, 
 			  7 : 31, 8 : 31, 9 : 30, 10 : 31, 11 : 30, 12 : 31 };
 			  
+function isLeapYear(year)
+{
+	if (year % 4 == 0)
+	{ 
+		if (year % 100 != 0)
+		{
+			return true;
+		}
+		else
+		{
+			if (year % 400 == 0)
+			{
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
+
 
