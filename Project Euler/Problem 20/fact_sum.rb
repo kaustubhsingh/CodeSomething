add87099fc0518cf764d7125c7ee6fc31d10009f
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-01-27 21:10:21 -0800

    problem 20

---
+++ Project Euler/Problem 20/fact_sum.rb
@@ -0,0 +1,24 @@
+
+def findDigitsAxB(a, b)
+	
+	digits = []
+	carry = 0
+	
+	while ((a + carry) > 0)
+		digit = (a + carry) % 10
+		a = (a + carry) / 10
+	
+		if ((digit * b + carry) < 10)
+			digits.insert(0, digit * b + carry)
+		else
+			digits.insert(0, (digit * b + carry) % 10)
+			carry = (digit * b) / 10
+		end
+		
+	end
+
+	return digits
+
+end
+
+puts findDigitsAxB(2, 5)
