d60f86640166391e70fa1923c43b3930d74b27d8
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-02-05 19:31:08 -0800

    code for findDivisors()

--- Project Euler/Problem 21/amicable_numbers.js
+++ Project Euler/Problem 21/amicable_numbers.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
 
+function findDivisors(n)
+{
+	var divisors = [];
+	
+	for (var i = 0; i <= n/2; i++)
+	{
+		if (n % i == 0)
+		{
+			divisors.push(i);
+		}
+	}
+	return divisors;
+}
+
