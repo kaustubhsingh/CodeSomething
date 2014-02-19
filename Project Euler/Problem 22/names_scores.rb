10cc45a5aef4e1f133933341c0b7954d1f4db0a7
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-02-18 20:10:45 -0800

    reading the names file

--- Project Euler/Problem 22/names_scores.rb
+++ Project Euler/Problem 22/names_scores.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/ruby
 
-f  = File.open("names.txt")
+f  = File.open("names_2.txt")
 
 array = []
 
 f.each_line do |x| 
 		x = x.split ","
+		puts x
+		puts "----"
 		array.insert(0, x)
 end
 
