e8226849ffc126daa4df7ecfc8a98e77fe64866c
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-02-24 19:40:08 -0800

    findAlphabeticalValue()

--- Project Euler/Problem 22/names_scores.rb
+++ Project Euler/Problem 22/names_scores.rb
@@ -21,14 +21,13 @@ def readNamesAndSort()
 	return names
 end
 
-findValue = { "A" => 1, "B" => 2, "C" => 3, "D" => 4, "E" => 5, "F" => 6,
+def findAlphabeticalValue(y)
+
+		findValue = { "A" => 1, "B" => 2, "C" => 3, "D" => 4, "E" => 5, "F" => 6,
 				"G" => 7, "H" => 8, "I" => 9, "J" => 10, "K" => 11, "L" => 12,
 				"M" => 13, "N" =>14, "O" => 15, "P" => 16, "Q" => 17, "R" => 18,
 				"S" => 19, "T" => 20, "U" => 21, "V" => 22, "W" => 23, "X" => 24,
 				"Y" => 25, "Z" => 26 }
-
-
-def findAlphabeticalValue(y)
 
 		counter = y.length
 		alphabetical_value = 0
@@ -37,7 +36,7 @@ def findAlphabeticalValue(y)
 		
 		y.split("").each{ |z|
 					if z =~ /[A-Z ]/
-					puts z.to_i
+						puts z
 						alphabetical_value += findValue[z]	
 					end	
 		}
