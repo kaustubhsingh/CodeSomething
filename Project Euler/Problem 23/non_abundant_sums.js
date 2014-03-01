ef78a30bc036b5a76f70c8dfc9b66763e47b78fa
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-03-01 11:53:25 -0800

    problem 23 isAbundant()

--- Project Euler/Problem 23/non_abundant_sums.js
+++ Project Euler/Problem 23/non_abundant_sums.js
@@ -2,3 +2,20 @@
 var findDivisors = require("./amicable_numbers.js");
 
 console.log(findDivisors(10));
+
+var isAbundant = function(n)
+{
+	var divisors = findDivisors(n);
+	
+	sum = 0;
+	for i in divisors
+	{
+		sum += i;
+	}
+	if (sum > n)
+		return true;
+	else
+		return false
+}
+
+console.log(isAbundant(10));
