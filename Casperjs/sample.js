20b0e3c82fb7c3e7de631be09445beb9b383fac3
Kaustubh Singh <kaustubh.singh@gmail.com>
2014-12-27 14:48:59 -0800

    Signed-off-by: Kaustubh Singh <kaustubh.singh@gmail.com>

--- Casperjs/sample.js
+++ Casperjs/sample.js
@@ -1,11 +1,21 @@
-var casper = require('casper').create();
+var casper = require('casper').create({ verbose: true, logLevel: "debug", waitTimeout: 6000});
 
-casper.start('http://casperjs.org/', function() {
-    this.echo(this.getTitle());
+
+
+// casper.thenOpen('http://phantomjs.org', function() {
+//     this.echo('URL is ' + this.getCurrentUrl());
+//     this.echo(this.getTitle());
+// });
+
+
+
+casper.start('', function() {
+  this.echo('URL is ' + this.getCurrentUrl());
+  this.echo(this.getTitle());
+
+
 });
 
-casper.thenOpen('http://phantomjs.org', function() {
-    this.echo(this.getTitle());
-});
+
 
 casper.run();
