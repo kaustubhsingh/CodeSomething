df6fe5d4d88c4b52f3bf86e71c101cbe7afb0d93
Kaustubh <kaustubh.singh@gmail.com>
2016-01-03 03:52:41 -0400

    update

---
+++ Angularjs/app.js
@@ -0,0 +1,30 @@
+
+(function(){
+    var app = angular.module('store', []); 
+    
+    app.controller('StoreController', function(){
+        this.product = gem;
+        
+        });
+    
+    var gem = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: '...',
+            canPurchase:true,
+            soldOut:false
+        },
+        {
+            name: 'Pentagonal gem',
+            price: 5.95,
+            description: '...',
+            canPurchase:true,
+            soldOut:true
+        },             
+
+    ];
+    
+})();
+
+
