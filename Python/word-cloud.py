ab0cd06209a5f5fa3c867ee2f14a96f7024e07e2
Kaustubh <kaustubh.singh@gmail.com>
2016-09-15 22:33:59 -0300

    update

--- Python/word-cloud.py
+++ Python/word-cloud.py
@@ -4,10 +4,21 @@
 class WordCount:
     def __init__(self, input_string):
         self.string = input_string
+        self.array  = []
+        
         
     def ExtractWords(self):
+		self.array = self.string.split()
+		return self
         
     def BuildDictionary(self):
+		dicti = {}
+		for i in self.array:
+			dicti[i] = 1
+		return dicti
+		
         
-        
-print WordCount("hello how are you").string
+
+a = WordCount("hello how are you").ExtractWords()    
+print (a.array)
+print (a.BuildDictionary())
