8489739a1a45c62f59bdb3c7b5d1b02b1b759650
Kaustubh <kaustubh.singh@gmail.com>
2016-09-28 22:53:35 -0300

    update

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -13,5 +13,32 @@ def fibonacci(n):
 		return dict[n]
 		
 		
-print fibonacci(10)
-print dict
+#print fibonacci(100)
+#print dict
+
+def fibo_iterative(n):
+	if n == 0 or n == 1:
+		return n
+		
+	fibo_n1 = 0
+	fibo_n2 = 1
+	
+	for ctr in range(n):
+		fibo_n = fibo_n1 + fibo_n2 
+		fibo_n2 = fibo_n1
+		fibo_n1 = fibo_n
+		
+	return fibo_n
+		
+print fibo_iterative(10)
+	
+def fibo_slow(n):
+	if n == 0 or n == 1:
+		return n
+	else:
+		return fibo_slow(n - 1) + fibo_slow(n - 2)
+		
+		
+print fibo_slow(10)
+
+
