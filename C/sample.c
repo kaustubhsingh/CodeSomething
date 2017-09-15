59dac1a32e90897e863f0127772e753608326583
Kaustubh Singh <kaustubh.singh@gmail.com>
2017-09-14 21:10:35 -0400

    update

--- C/sample.c
+++ C/sample.c
@@ -3,7 +3,31 @@
 
 #include <stdio.h>
 
+typedef struct list {
+    int item;
+    struct list *next;
+
+} list;
+
+
 int main(void) {
   printf("Hello World!\n");
+
+  list a, b;
+  a.item = 5;
+  a.next = &b;
+  b.item = 6;
+  b.next = NULL;
+
+  list *c;
+  c = &a;
+
+  while (c)
+  {
+    printf("%d", c->item);
+    c = c->next;
+
+  }
+
   // return 0; 
 }
